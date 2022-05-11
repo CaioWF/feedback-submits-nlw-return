4d173d67a2935561fdc17bfe68ab0a48edb5e41d
caiowf <contato.caioweliton@gmail.com>
2022-05-11 00:13:39 -0300

    fix: :rocket: add port variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes)
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
